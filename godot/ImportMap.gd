57cd7b9bdfc9bf430d5ff92e386a70002c75b901
Teodor Potancok <teodor.potancok@gmail.com>
2025-05-20 10:23:21 +0200

    Handle Blender duplicate nodes (.001)

--- godot/ImportMap.gd
+++ godot/ImportMap.gd
@@ -12,22 +12,31 @@ func replace_node(template: Node, new_node: Node, new_name: String) -> void:
 	if parent.has_node(new_name):
 		parent.get_node(new_name).free()
 	parent.add_child(new_node)
+	new_node.name = new_name
 	new_node.owner = self.owner
 	template.hide()
 
 func handle_node(node: Node) -> void:
 	if node.name.begins_with("$"):
 		var real_name := node.name.trim_prefix("$")
+		var scene_name := real_name
+		var right := real_name.right(4)
+		if right[0] == "_":
+			var tag := right.right(3)
+			if tag.is_valid_int():
+				# remove blender ".001"
+				scene_name = real_name.left(-4)
+
 		print("Spawning entity: ", real_name)
 		var scene: PackedScene
-		if FileAccess.file_exists("res://scenes/%s.tscn" % real_name):
+		if FileAccess.file_exists("res://scenes/%s.tscn" % scene_name):
 			# text
-			scene = load("res://scenes/%s.tscn" % real_name)
-		elif FileAccess.file_exists("res://scenes/%s.scn" % real_name):
+			scene = load("res://scenes/%s.tscn" % scene_name)
+		elif FileAccess.file_exists("res://scenes/%s.scn" % scene_name):
 			# binary
-			scene = load("res://scenes/%s.scn" % real_name)
+			scene = load("res://scenes/%s.scn" % scene_name)
 		else:
-			printerr("Scene with name \"%s\" doesn't exist in res://scenes" % real_name)
+			printerr("Scene with name \"%s\" doesn't exist in res://scenes" % scene_name)
 
 		var new_node := scene.instantiate()
 		replace_node(node, new_node, real_name)
